bd9aae0a166ac4324c2a4335bbc81dcdfeddb075
AlexZ005 <alexz005.info@gmail.com>
2018-03-30 15:09:16 +0200

    exporting xmodmap contents to advanced window

--- xmkeys_advanced.cpp
+++ xmkeys_advanced.cpp
@@ -2,7 +2,9 @@
 #include "ui_xmkeys_advanced.h"
 #include <QDebug>
 #include <QVBoxLayout>
+#include <QProcess>
 
+QString adverr;
 xmkeys_advanced::xmkeys_advanced(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::xmkeys_advanced)
@@ -10,17 +12,28 @@ xmkeys_advanced::xmkeys_advanced(QWidget *parent) :
     ui->setupUi(this);
     qDebug() << "advanced window is here";
 
+    ///proces
+    QProcess process3;
+    process3.start("sh", QStringList() << "-c" << "xmodmap -pke");
+    process3.waitForFinished();
+    adverr = process3.readAllStandardOutput();
+
     QVBoxLayout * lay = new QVBoxLayout(this);
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            QPushButton* panelButton = new QPushButton();
-            panelButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    QRegularExpression re("(\\w+)");
+    QRegularExpressionMatchIterator i = re.globalMatch(adverr);
+    QStringList words;
+    while (i.hasNext()) {
+        QRegularExpressionMatch match = i.next();
+        QString word = match.captured(1);
+        words << word;
+        qDebug() << "matched " << word;
+        QPushButton* panelButton = new QPushButton();
+        panelButton->setText(word);
+        panelButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 //            ui.PanelButtonGridLayout->addWidget(panelButton,i,j);
-            lay->addWidget(panelButton,i);
-        }
+        lay->addWidget(panelButton);
     }
+
     ui->scrollAreaWidgetContents->setLayout(lay);
 
 }
